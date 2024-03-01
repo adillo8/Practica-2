43b15c54e06842a897150f73b1476bc7deded43c
adillo8 <pilar.fernandezadillo@usp.ceu.es>
2024-03-01 08:25:33 +0100

    "error arreglado :3"

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -2,7 +2,7 @@ package aplicacion;
 import dominio.Aproximacion;
 public class Principal {
     public static void main(String[] args) {
-        int totalDardos = 10000;
+        int totalDardos = 1000;
         double piAproximado = Aproximacion.aproximarPi(totalDardos);
         System.out.println("Aproximación de pi lanzando " + totalDardos + " dardos: " + piAproximado);
     }
